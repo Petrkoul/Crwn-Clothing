89c6bc1ba2b2ca2191d1cc79e0e0cd5491a93efb
Petrkoul <koulpetran@yahoo.gr>
2023-11-16 17:45:49 +0200

    sign-up

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,22 @@
+// import { useEffect  } from "react";
+// import {  getRedirectResult } from 'firebase/auth'
+
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
+// import { auth, signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+
+  // useEffect( () => {
+  //   async () => {
+  //   const response = await getRedirectResult(auth);
+    
+  //   if (response) {
+  //     const userDocRef = await createUserDocumentFromAuth(response.user);
+  //   }
+  // }, []});
 
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
@@ -14,6 +29,10 @@ const SignIn = () => {
       <button onClick={logGoogleUser}>
         Sign in with Google Popup
       </button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
+      <SignUpForm />
     </div>
   )
 }
