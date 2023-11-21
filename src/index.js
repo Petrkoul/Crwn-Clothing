3ddb54d3f8f0a72286a98024cde23cccb166267f
Petrkoul <koulpetran@yahoo.gr>
2023-11-21 19:24:42 +0200

    checkout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { UserProvider } from './contexts/user.context';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
+import './index.scss';
+
+import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,16 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom"
-
-import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import { Outlet, Link } from "react-router-dom";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-import './navigation.styles.scss'
+import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext)
 
   //console.log(currentUser);
   return (
@@ -28,7 +31,9 @@ const Navigation = () => {
                 SIGN IN
               </Link>)
           }
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
